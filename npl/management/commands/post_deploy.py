3ca69e1c395d905edbb9b553f1e1da31139d1388
Jeremy Bowers <jeremyjbowers@gmail.com>
2023-07-22 21:04:33 -0400

    don't load fixtures on deploy

--- npl/management/commands/post_deploy.py
+++ npl/management/commands/post_deploy.py
@@ -9,5 +9,5 @@ from npl import models, utils
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        call_command('loaddata', 'data/fixtures/users.json')
-        call_command('loaddata', 'data/fixtures/npl.json')
+        # call_command('loaddata', 'data/fixtures/users.json')
+        # call_command('loaddata', 'data/fixtures/npl.json')
