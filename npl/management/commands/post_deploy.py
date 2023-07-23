afe18aaebde85a40f36ccf109243efc70d853b70
Jeremy Bowers <jeremyjbowers@gmail.com>
2023-07-22 20:45:31 -0400

    loaddata on deploy

---
+++ npl/management/commands/post_deploy.py
@@ -0,0 +1,13 @@
+from django.apps import apps
+from django.db import connection
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from npl import models, utils
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        call_command('loaddata', 'data/fixtures/users.json')
+        call_command('loaddata', 'data/fixtures/npl.json')
