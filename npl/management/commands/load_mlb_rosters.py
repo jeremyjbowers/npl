e14ec17fc77bfe7b562a091f8f2b327f718d4f73
Jeremy Bowers <jeremyjbowers@gmail.com>
2023-08-19 14:00:42 -0400

    more defense

--- npl/management/commands/load_mlb_rosters.py
+++ npl/management/commands/load_mlb_rosters.py
@@ -8,58 +8,67 @@ from npl import models, utils
 
 
 class Command(BaseCommand):
+    headers = {
+        "Referer": "https://www.milb.com/",
+        "Sec-Ch-Ua": '"Not/A)Brand";v="99", "Microsoft Edge";v="115", "Chromium";v="115"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "macOS",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.203",
+    }
     def get_cpx_rosters(self):
         roster_urls = []
         for league_url in [self.FCL_URL, self.AZL_URL, self.DSL_URL]:
-            r = requests.get(league_url)
+            r = requests.get(league_url, headers=self.headers)
             soup = BeautifulSoup(r.content, 'html.parser')
             for a in soup.select('a'):
                 if a.get('href', None):
-                    if 'roster' in a.attrs['href']:
+                    if '/roster' in a.attrs['href']:
                         roster_urls.append(a.attrs['href'])
 
         for url in roster_urls:
-            tr = requests.get(url)
-            ts = BeautifulSoup(tr.content, 'html.parser')
-            player_rows = ts.select('div.players tr')
+            tr = requests.get(url, headers=self.headers)
 
-            for row in player_rows:
-                cells = row.select('td')
-                player_dict = None
-                try:
-                    player_dict = {}
-                    player_dict['name'] = cells[1].select('a')[0].text.strip()
-                    player_dict['mlb_id'] = cells[1].select('a')[0].attrs['href'].split('/')[-1].strip()
-                    year = cells[5].text.strip().split('/')[2]
-                    month = cells[5].text.strip().split('/')[0].zfill(2)
-                    day = cells[5].text.strip().split('/')[1].zfill(2)
-                    player_dict['birthdate'] = f"{year}-{month}-{day}"
-                    player_dict['roster_status'] = "MINORS"
+            if tr.status_code == 200:
+                ts = BeautifulSoup(tr.content, 'html.parser')
+                player_rows = ts.select('div.players tr')
 
-                    if "Injured 7" in cells[6].text.strip():
-                        player_dict['roster_status'] = "IL-7"
-
-                    if "Injured 60" in cells[6].text.strip():
-                        player_dict['roster_status'] = "IL-60"
-
-                except:
-                    pass
-                    
-                if player_dict:
+                for row in player_rows:
+                    cells = row.select('td')
+                    player_dict = None
                     try:
-                        obj = models.Player.objects.get(mlb_id=player_dict['mlb_id'])
-                    
-                    except models.Player.DoesNotExist:
-                        obj = models.Player()
-                    
-                    for k,v in player_dict.items():
-                        setattr(obj, k, v)
+                        player_dict = {}
+                        player_dict['name'] = cells[1].select('a')[0].text.strip()
+                        player_dict['mlb_id'] = cells[1].select('a')[0].attrs['href'].split('/')[-1].strip()
+                        year = cells[5].text.strip().split('/')[2]
+                        month = cells[5].text.strip().split('/')[0].zfill(2)
+                        day = cells[5].text.strip().split('/')[1].zfill(2)
+                        player_dict['birthdate'] = f"{year}-{month}-{day}"
+                        player_dict['roster_status'] = "MINORS"
 
-                    obj.save()
-                    print(obj)
+                        if "Injured 7" in cells[6].text.strip():
+                            player_dict['roster_status'] = "IL-7"
+
+                        if "Injured 60" in cells[6].text.strip():
+                            player_dict['roster_status'] = "IL-60"
+
+                    except:
+                        pass
+                        
+                    if player_dict:
+                        try:
+                            obj = models.Player.objects.get(mlb_id=player_dict['mlb_id'])
+                        
+                        except models.Player.DoesNotExist:
+                            obj = models.Player()
+                        
+                        for k,v in player_dict.items():
+                            setattr(obj, k, v)
+
+                        obj.save()
+                        print(obj)
 
     def get_milb_rosters(self):
-        r = requests.get(self.MILB_AFFILIATE_URL)
+        r = requests.get(self.MILB_AFFILIATE_URL, headers=self.headers)
         soup = BeautifulSoup(r.content, 'html.parser')
         team_urls = [a.attrs['href'] for a in soup.select('a.p-forge-logo--link') if "www" in a.attrs['href']]
         for url in team_urls:
@@ -70,46 +79,50 @@ class Command(BaseCommand):
             if url == "https://www.saintsbaseball.com/":
                 url = "https://www.milb.com/st-paul"
 
-            tr = requests.get(f"{url}/roster")
-            ts = BeautifulSoup(tr.content, 'html.parser')
-            org = ts.select('button.tabs__filter-buttons--button')[2].text.split(' Transactions')[0].strip()
+            print(f"{url}/roster")
 
-            player_rows = ts.select('div.players tr')
-            for row in player_rows:
-                cells = row.select('td')
-                player_dict = None
-                try:
-                    player_dict = {}
-                    player_dict['name'] = cells[1].select('a')[0].text.strip()
-                    player_dict['mlb_id'] = cells[1].select('a')[0].attrs['href'].split('/')[-1].strip()
-                    year = cells[5].text.strip().split('/')[2]
-                    month = cells[5].text.strip().split('/')[0].zfill(2)
-                    day = cells[5].text.strip().split('/')[1].zfill(2)
-                    player_dict['birthdate'] = f"{year}-{month}-{day}"
-                    player_dict['roster_status'] = "MINORS"
-                    player_dict['mlb_org'] = org
+            tr = requests.get(f"{url}/roster", headers=self.headers)
 
-                    if "Injured 7" in cells[6].text.strip():
-                        player_dict['roster_status'] = "IL-7"
+            if tr.status_code == 200:
+                ts = BeautifulSoup(tr.content, 'html.parser')
+                org = ts.select('button.tabs__filter-buttons--button')[2].text.split(' Transactions')[0].strip()
 
-                    if "Injured 60" in cells[6].text.strip():
-                        player_dict['roster_status'] = "IL-60"
-
-                except:
-                    pass
-                    
-                if player_dict:
+                player_rows = ts.select('div.players tr')
+                for row in player_rows:
+                    cells = row.select('td')
+                    player_dict = None
                     try:
-                        obj = models.Player.objects.get(mlb_id=player_dict['mlb_id'])
-                    
-                    except models.Player.DoesNotExist:
-                        obj = models.Player()
-                    
-                    for k,v in player_dict.items():
-                        setattr(obj, k, v)
+                        player_dict = {}
+                        player_dict['name'] = cells[1].select('a')[0].text.strip()
+                        player_dict['mlb_id'] = cells[1].select('a')[0].attrs['href'].split('/')[-1].strip()
+                        year = cells[5].text.strip().split('/')[2]
+                        month = cells[5].text.strip().split('/')[0].zfill(2)
+                        day = cells[5].text.strip().split('/')[1].zfill(2)
+                        player_dict['birthdate'] = f"{year}-{month}-{day}"
+                        player_dict['roster_status'] = "MINORS"
+                        player_dict['mlb_org'] = org
 
-                    obj.save()
-                    print(obj)
+                        if "Injured 7" in cells[6].text.strip():
+                            player_dict['roster_status'] = "IL-7"
+
+                        if "Injured 60" in cells[6].text.strip():
+                            player_dict['roster_status'] = "IL-60"
+
+                    except:
+                        pass
+                        
+                    if player_dict:
+                        try:
+                            obj = models.Player.objects.get(mlb_id=player_dict['mlb_id'])
+                        
+                        except models.Player.DoesNotExist:
+                            obj = models.Player()
+                        
+                        for k,v in player_dict.items():
+                            setattr(obj, k, v)
+
+                        obj.save()
+                        print(obj)
 
     def get_mlb_rosters(self):
         r = requests.get(self.MLB_DEPTH_URL)
@@ -118,43 +131,45 @@ class Command(BaseCommand):
         team_urls = [a.attrs['href'] for a in soup.select('a') if "roster" in a.attrs['href'] and "www" in a.attrs['href']]
 
         for url in team_urls:
-            tr = requests.get(url.replace('depth-chart', '40-man'))
-            ts = BeautifulSoup(tr.content, 'html.parser')
+            tr = requests.get(url.replace('depth-chart', '40-man'), headers=self.headers)
 
-            org = settings.MLB_URL_TO_ORG_NAME.get(url.split('mlb.com/')[1].split('/roster')[0].strip(), None)
+            if tr.status_code == 200:
+                ts = BeautifulSoup(tr.content, 'html.parser')
 
-            player_rows = ts.select('div.players tbody tr')
-            for row in player_rows:
-                cells = row.select('td')
-                player_dict = None
+                org = settings.MLB_URL_TO_ORG_NAME.get(url.split('mlb.com/')[1].split('/roster')[0].strip(), None)
 
-                player_dict = {}
-                player_dict['name'] = cells[1].select('a')[0].text.strip()
-                player_dict['roster_status'] = "MLB"
+                player_rows = ts.select('div.players tbody tr')
+                for row in player_rows:
+                    cells = row.select('td')
+                    player_dict = None
 
-                for span in cells[1].select('span'):
-                    if "status" in span.attrs['class'][0]:
-                        player_dict['roster_status'] = cells[1].select('span')[1].text.strip().upper()
+                    player_dict = {}
+                    player_dict['name'] = cells[1].select('a')[0].text.strip()
+                    player_dict['roster_status'] = "MLB"
 
-                player_dict['mlb_id'] = cells[1].select('a')[0].attrs['href'].split('-')[-1].strip()
-                year = cells[5].text.strip().split('/')[2]
-                month = cells[5].text.strip().split('/')[0].zfill(2)
-                day = cells[5].text.strip().split('/')[1].zfill(2)
-                player_dict['birthdate'] = f"{year}-{month}-{day}"
-                player_dict['mlb_org'] = org
-                    
-                if player_dict:
-                    try:
-                        obj = models.Player.objects.get(mlb_id=player_dict['mlb_id'])
-                    
-                    except models.Player.DoesNotExist:
-                        obj = models.Player()
-                    
-                    for k,v in player_dict.items():
-                        setattr(obj, k, v)
+                    for span in cells[1].select('span'):
+                        if "status" in span.attrs['class'][0]:
+                            player_dict['roster_status'] = cells[1].select('span')[1].text.strip().upper()
 
-                    obj.save()
-                    print(obj)
+                    player_dict['mlb_id'] = cells[1].select('a')[0].attrs['href'].split('-')[-1].strip()
+                    year = cells[5].text.strip().split('/')[2]
+                    month = cells[5].text.strip().split('/')[0].zfill(2)
+                    day = cells[5].text.strip().split('/')[1].zfill(2)
+                    player_dict['birthdate'] = f"{year}-{month}-{day}"
+                    player_dict['mlb_org'] = org
+                        
+                    if player_dict:
+                        try:
+                            obj = models.Player.objects.get(mlb_id=player_dict['mlb_id'])
+                        
+                        except models.Player.DoesNotExist:
+                            obj = models.Player()
+                        
+                        for k,v in player_dict.items():
+                            setattr(obj, k, v)
+
+                        obj.save()
+                        print(obj)
 
     def handle(self, *args, **options):
 
