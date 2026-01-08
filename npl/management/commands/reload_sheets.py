f721ca365db0921a6097d3cdf4a91c076934609f
Jeremy Bowers <jeremyjbowers@gmail.com>
2026-01-08 12:57:44 -0500

    ready to put the full command back

--- npl/management/commands/reload_sheets.py
+++ npl/management/commands/reload_sheets.py
@@ -9,11 +9,11 @@ from npl import models, utils
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # # Update the universe of players from MLB
-        # call_command('load_mlb_rosters')
+        # Update the universe of players from MLB
+        call_command('load_mlb_rosters')
 
-        # # Update NPL specific data
-        # call_command('initial_load_teams')
+        # Update NPL specific data
+        call_command('initial_load_teams')
         call_command('initial_load_npl_roster_sheet')
         call_command('initial_load_contracts')
         call_command('initial_load_transactions')
